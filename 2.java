7956afcf661004fbf83c28ca4f11dcf684285b18
SaranKumar516 <saran.kamandula999@gmail.com>
2024-06-08 01:44:54 +0000

    Unable to merge

--- 2.java
+++ 2.java
@@ -2,9 +2,18 @@ this is a new java fil
 
 second stash
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 
 this is an update to test the stash
 
 
 
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
